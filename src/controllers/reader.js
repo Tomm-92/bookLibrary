9c4783d611798e21db3f034a5f96dba57683950c
Tom Wilkinson <thomasmarkwilkinson@gmail.com>
2023-02-26 17:49:08 +0000

    working on CRUD tests - up to sending error on id

--- src/controllers/reader.js
+++ src/controllers/reader.js
@@ -5,6 +5,17 @@ const createUser = async (req, res) => {
   res.status(201).json(newReader);
 };
 
+const allUsers = async (req, res) => {
+    const newReader = await Reader.findAll();
+    res.status(200).json(newReader);
+}
+
+const userById = async (req, res) => {
+    const {id} = req.params
+    const newReader = await Reader.findByPk(id);
+    res.status(200).json(newReader);
+}
+
 module.exports = {
-  createUser,
+  createUser, allUsers, userById
 };
--- src/routes/reader.js
+++ src/routes/reader.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const readerController = require('../controllers/reader');
 
 router.post('/', readerController.createUser);
+router.get('/', readerController.allUsers);
+router.get('/:id', readerController.userById);
+
 
 module.exports = router;
